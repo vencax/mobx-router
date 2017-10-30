a2cceed1bce5c13b5c4803a2f9ec461833f53eba
Vaclav Klecanda <vencax77@gmail.com>
2017-10-30 15:25:17 +0100

    origParams saved before set to this.router

--- src/router-store.js
+++ src/router-store.js
@@ -32,17 +32,20 @@ class RouterStore {
     if (beforeEnterResult === false) {
       return;
     }
-
-    rootViewChanged && this.currentView && this.currentView.onExit && this.currentView.onExit(this.currentView, currentParams, store, currentQueryParams);
-
-    this.currentView = view;
-    this.params = toJS(paramsObj);
-    this.queryParams = toJS(queryParamsObj);
     const nextParams = toJS(paramsObj);
     const nextQueryParams = toJS(queryParamsObj);
 
+    rootViewChanged && this.currentView && this.currentView.onExit && this.currentView.onExit(this.currentView, currentParams, store, currentQueryParams);
+
+    const origParams = toJS(this.params)
+    const origQueryParams = toJS(this.queryParams)
+    this.currentView = view;
+    this.params = toJS(paramsObj);
+    this.queryParams = toJS(queryParamsObj);
+
     rootViewChanged && view.onEnter && view.onEnter(view, nextParams, store, nextQueryParams);
-    !rootViewChanged && this.currentView && this.currentView.onParamsChange && this.currentView.onParamsChange(this.currentView, nextParams, store, nextQueryParams);
+    !rootViewChanged && this.currentView && this.currentView.onParamsChange &&
+      this.currentView.onParamsChange(this.currentView, origParams, store, origQueryParams);
   }
 
   @computed get currentPath() {
